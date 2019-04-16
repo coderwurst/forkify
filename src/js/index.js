5fec56cc5fee7eb4cb6c1a025bd3255db718aeba
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-04-16 17:37:20 +0200

    remove test code

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-// Global app controller
-import num from "./testwebpack";
-const x = 23;
-
-console.log(`index.js imported ${num} from the testwebpack module! Var x is ${x}`);
--- src/js/testwebpack.js
+++
@@ -1,2 +0,0 @@
-console.log('imported module');
-export default 12345;                  // ES6 Module syntax
