a5c79673c3e53c7addd7a4960c28984e0a035711
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-04-16 20:44:38 +0200

    getResults in Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search'
 
-const key = '2f9b544a5fdeb04bb2dc4373ead448b6';
-const searchURL = 'https://www.food2fork.com/api/search';
+const search = new Search('pizza');
 
-async function getResults(query) {
-    try {
-        const result = await axios(`${searchURL}?key=${key}&q=${query}`);
-        console.log(result);
-    } catch (error) {
-        alert(error);
-    }
-}
-getResults('pizza');
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor (query) {
+        this.query = query;  
+    }
+
+    async getResults() {
+        const key = '2f9b544a5fdeb04bb2dc4373ead448b6';
+        const searchURL = 'https://www.food2fork.com/api/search';
+
+        try {
+            const result = await axios(`${searchURL}?key=${key}&q=${this.query}`);
+            console.log(result);
+            this.results = result.data.recipies;
+            console.log(this.results);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
