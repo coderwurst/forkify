de8d35ba2dca361c856781aad0612deb21e903e3
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-04-29 21:30:53 +0200

    shopping list view

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -5,7 +5,8 @@ export const elements = {
     searchResults: document.querySelector('.results'),
     recipeArea: document.querySelector('.recipe'),
     searchResultsPages: document.querySelector('.results__pages'),
-    recipe: document.querySelector('.recipe')
+    recipe: document.querySelector('.recipe'),
+    shopping: document.querySelector('.shopping__list')
 };
 
 export const elementStrings = {
